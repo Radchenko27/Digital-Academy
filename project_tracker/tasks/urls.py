a1fcac297d6f0b6cc2386e60f1e58c17e3479c9a
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-04-05 01:32:47 +0300

    Панель администрирования Django и Диспетчер URL

---
+++ project_tracker/tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'tasks'
+urlpatterns = [
+    path('', views.index),
+    path("another/", views.another_page, name="another_page"), #новый маршрут
+]
