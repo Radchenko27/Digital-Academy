9cdf2088ecce13e5a8c26a423de503b88eb38b07
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-04-06 01:48:39 +0300

    Представления на основе классов и функции

--- project_tracker/project_tracker/urls.py
+++ project_tracker/project_tracker/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from tasks.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("task/", index),
     path("", include("tasks.urls")),
 ]
+
